52d610da2c3726b4f863391165954ca9494df5d7
DpkRn <dwizardtechno@gmail.com>
2024-11-14 13:27:52 +0530

    asdfgh

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -74,7 +74,7 @@ function Login() {
                       </div>
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
-                  <button type="submit" className="w-full text-white bg-slate-500 hover:bg-primary-700 focus:ring-4 focus:outline-none flex justify-center items-center focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={(e)=>{
+                  <button  className="w-full text-white bg-slate-500 hover:bg-primary-700 focus:ring-4 focus:outline-none flex justify-center items-center focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={(e)=>{
                     login(e)
                   }}>
                      {loader&&<svg class="animate-spin h-5 w-5 mr-3 ... bg-white " viewBox="0 0 24 24"></svg>}
